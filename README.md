0a53fd061c146cdb75b474432f7c45747a20a26a
Min-ki Park <browneyedsoul@yonsei.ac.kr>
2023-01-24 16:07:04 +0900

    2nd commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# RemNote Aligner
+
+Simple yet necessary things for every gardener who really cares about document layout
+
+## Usage
+
+- Slash/Omnibar command → Type `ce` or `ri` → Return(Enter)
+
+| Alignment |Tag|
+|---|---|
+|center|`Center`|
+|right|`Right`|
--- src/widgets/index.tsx
+++ src/widgets/index.tsx
@@ -1,4 +1,4 @@
-import { declareIndexPlugin, ReactRNPlugin } from "@remnote/plugin-sdk";
+import {declareIndexPlugin, ReactRNPlugin} from "@remnote/plugin-sdk";
 
 /**
  * Simple example snippet plugin which shows how to:
@@ -7,64 +7,51 @@ import { declareIndexPlugin, ReactRNPlugin } from "@remnote/plugin-sdk";
  * - Register a command
  *
  * How to Use:
- * - Tag a Rem with ##Plugin Style, or use the /Add Plugin Style command on a Rem
+ * - Tag a Rem with ##Right, or use the /Add Right command on a Rem
  * - The Rem will be styled with the CSS defined in the plugin
  */
+export const [CENTER, RIGHT] = ["center_powerup", "right_powerup"];
+
 async function onActivate(plugin: ReactRNPlugin) {
-  // Register a setting to change the Rem's text color
-  await plugin.settings.registerStringSetting({
-    id: "color",
-    title: "Text Color (hex)",
-    description: "Provide a hex color for the text",
-    defaultValue: "#ff0000",
-  });
-
-  // Each time the setting changes, re-register the text color css.
-  plugin.track(async (reactivePlugin) => {
-    const color = await reactivePlugin.settings.getSetting("color");
-    await reactivePlugin.app.registerCSS(
-      "color",
-      `[data-rem-tags~="plugin-style"] { color: ${color}; }`
-    );
-  });
-
-  // Register a setting to change the Rem's margin
-  await plugin.settings.registerStringSetting({
-    id: "margin",
-    title: "Rem Margin (px)",
-    description: "Provide a margin for the Rem",
-    defaultValue: "10",
-  });
-
-  // Each time the margin setting changes, re-register the margin css.
-  plugin.track(async (reactivePlugin) => {
-    const margin = await reactivePlugin.settings.getSetting<string>("margin");
-    try {
-      const marginAsNumber = Number.parseInt(margin);
-      await reactivePlugin.app.registerCSS(
-        "margin",
-        `[data-rem-tags~="plugin-style"] { margin: ${marginAsNumber}px; }`
-      );
-    } catch {}
-  });
-
-  // A command that adds a style tag to the current focused Rem.
+  await plugin.app.registerPowerup("Center", CENTER, "A Power-up Block for aligning inline item", {slots: []});
+  await plugin.app.registerPowerup("Right", RIGHT, "A Power-up Block for aligning inline item", {slots: []});
+  
   await plugin.app.registerCommand({
-    id: "add-style-tag",
-    name: "Add Plugin Style",
-    description: "Add a style tag to the current focused Rem",
+    id: "center",
+    name: "Center",
+    description: "Align items per command",
     action: async () => {
       const focusedRem = await plugin.focus.getFocusedRem();
       if (!focusedRem) {
         return;
       }
-      let tag = await plugin.rem.findByName(["Plugin Style"], null);
+      let tag = await plugin.rem.findByName(["Center"], null);
       if (!tag) {
         tag = await plugin.rem.createRem();
         if (!tag) {
           return;
         }
-        await tag.setText(["Plugin Style"]);
+        await tag.setText(["Center"]);
+      }
+      await focusedRem.addTag(tag);
+    },
+  });
+  await plugin.app.registerCommand({
+    id: "right",
+    name: "Right",
+    description: "Align items per command",
+    action: async () => {
+      const focusedRem = await plugin.focus.getFocusedRem();
+      if (!focusedRem) {
+        return;
+      }
+      let tag = await plugin.rem.findByName(["Right"], null);
+      if (!tag) {
+        tag = await plugin.rem.createRem();
+        if (!tag) {
+          return;
+        }
+        await tag.setText(["Right"]);
       }
       await focusedRem.addTag(tag);
     },
